474442b861bc403a898202515dd5c447f5d5f0e0
Morogui <moro.gui@icloud.com>
2026-09-02 03:07:58 -0300

    Add metadata to page export
    Add metadata export for page with canonical URL.

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
+import type { Metadata } from "next";
+
 export { default } from "./painel/page";
+
+export const metadata: Metadata = {
+    alternates: {
+          canonical: "https://www.escala7x7ecommerce.com.br/painel",
+    },
+};
