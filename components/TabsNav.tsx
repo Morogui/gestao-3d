2671063b9d37af6b80457b2347409bcbfe2a7cdc
Morogui <moro.gui@icloud.com>
2026-09-14 21:55:33 -0300

    Add 'Precificação' tab to TabsNav component

--- components/TabsNav.tsx
+++ components/TabsNav.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 
 const TABS = [
   { href: "/custo", label: "Custo" },
+  { href: "/precificacao", label: "Precificação" },
   { href: "/vendas", label: "Vendas" },
   { href: "/producao", label: "Produção" },
   { href: "/estoque", label: "Estoque" },
